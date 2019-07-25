50ddfe4cb468af1669a6a581f57a2e1e6aa3db7f
Ksoft0081 <phamphuckhai@gmail.com>
2019-07-26 01:32:04 +0700

    Add Class Ground And Tileset Map Pittsburgh Light

---
+++ Captain-America-And-The-Avengers/Ground.cpp
@@ -0,0 +1,35 @@
+//#include "Ground.h"
+//
+//Ground::Ground(D3DXVECTOR3 position) : Entity()
+//{
+//	SetPosition(position);
+//	//Quy dinh ground la 16x16
+//	SetWidth(16.0f);
+//	SetHeight(16.0f);
+//	
+//}
+//
+//void Ground::GetBoundingBox(float &left, float &top, float &right, float &bottom)//Dung get box xet va cham trong sweep aabb
+//{
+//	left = GetPosition().x;
+//	top = GetPosition().y;
+//	right = left + mWidth;
+//	bottom = top + mHeight;
+//}
+//void Ground::Update()
+//{
+//
+//}
+//void Ground::Draw()
+//{
+//
+//}
+//Ground::~Ground()
+//{
+//
+//}
+//void Ground::LoadSpriteBoudingBox()
+//{
+//	//Load anh trong resource//sprite//boudingbox
+//}
+
---
+++ Captain-America-And-The-Avengers/Ground.h
@@ -0,0 +1,25 @@
+//#pragma once
+//#include"Game.h"
+//#include "pch.h"
+//#include "Entity.h"
+//#include "Camera.h"
+//
+//
+//class Ground : public Entity
+//{
+//public:
+//
+//	Ground(D3DXVECTOR3 position);
+//
+//	~Ground();
+//
+//	void Update();
+//	void Draw();
+//	void LoadSpriteBoudingBox();//Dung load bouding box test va cham
+//
+//
+//	void GetBoundingBox(float &left, float &top, float &right, float &bottom);
+//
+//	/*void RenderBoundingBox(D3DXVECTOR2 transform);*/
+//};
+//
