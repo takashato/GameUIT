73a7580487a45935ec5b8a8650eb50de9022e686
Thanh Sơn Bành <takashato@gmail.com>
2019-08-02 23:37:05 +0700

    Remove dxutil anoying warnings

--- Captain-America-And-The-Avengers/dxutil.h
+++ Captain-America-And-The-Avengers/dxutil.h
@@ -17,9 +17,15 @@
 //-----------------------------------------------------------------------------
 // Miscellaneous helper functions
 //-----------------------------------------------------------------------------
+#ifndef SAFE_DELETE
 #define SAFE_DELETE(p)       { if(p) { delete (p);     (p)=NULL; } }
+#endif
+#ifndef SAFE_DELETE_ARRAY
 #define SAFE_DELETE_ARRAY(p) { if(p) { delete[] (p);   (p)=NULL; } }
+#endif
+#ifndef SAFE_RELEASE
 #define SAFE_RELEASE(p)      { if(p) { (p)->Release(); (p)=NULL; } }
+#endif
 
 
 //-----------------------------------------------------------------------------
