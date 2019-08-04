73b50604f166cd6c4ed209d709ffbb00d823bcfd
Bành Thanh Sơn <takashato@gmail.com>
2019-08-04 22:59:53 +0700

    Handle collision for PlayerSitOnShield state

--- Captain-America-And-The-Avengers/PlayerSitOnShield.cpp
+++ Captain-America-And-The-Avengers/PlayerSitOnShield.cpp
@@ -1,6 +1,7 @@
 #include "pch.h"
 #include "Player.h"
 #include "PlayerSitOnShield.h"
+#include "Ground.h"
 
 void PlayerSitOnShield::Enter(Player& player, EPlayerState fromState, Data&& data)
 {
@@ -24,6 +25,11 @@ void PlayerSitOnShield::HandleKeyboard(Player& player, Keyboard* keyboard)
 {
 	if (keyboard->IsPressing(GAME_KEY_LEFT))
 	{
+		if (player.mIsCollisionLeftRightSide && player.mDirectionUnblock == Left)
+		{
+			player.mIsCollisionLeftRightSide = false;
+		}
+
 		if (player.GetVelocityX() >= 0)
 		{
 			player.SetVelocityX(-MOVING_HOR);
@@ -32,6 +38,11 @@ void PlayerSitOnShield::HandleKeyboard(Player& player, Keyboard* keyboard)
 	}
 	else if (keyboard->IsPressing(GAME_KEY_RIGHT))
 	{
+		if (player.mIsCollisionLeftRightSide && player.mDirectionUnblock == Right)
+		{
+			player.mIsCollisionLeftRightSide = false;
+		}
+
 		if (player.GetVelocityX() >= 0)
 		{
 			player.SetVelocityX(MOVING_HOR);
@@ -64,6 +75,41 @@ void PlayerSitOnShield::OnKeyUp(Player& player, BYTE code)
 
 void PlayerSitOnShield::OnCollision(Player& player, std::vector<CollisionEvent*>& cEvent)
 {
+	bool collisionWithGround = false;
+	CollisionEvent* groundCe = nullptr;
+	for (auto ce : cEvent)
+	{
+		if (ce->entity->GetCollidableObjectType() == EPlatform)
+		{
+			if (ce->ny == -1.0f)
+			{
+				collisionWithGround = true;
+				groundCe = ce;
+			}
+
+			if (((Ground*)ce->entity)->GetGroundType() == EGroundHard)
+			{
+				if (ce->nx == -1.0f)
+				{
+					player.mIsCollisionLeftRightSide = true;
+					player.mDirectionUnblock = Left;
+					player.SetPositionX(ce->entity->GetPosition().x - player.GetWidth());
+				}
+				else if (ce->nx == 1.0f)
+				{
+					player.mIsCollisionLeftRightSide = true;
+					player.mDirectionUnblock = Right;
+					player.SetPositionX(ce->entity->GetBoundingBox().right);
+				}
+			}
+		}
+	}
+	if (collisionWithGround)
+	{
+		SoundManager::GetInstance().CPlaySound(SoundType::SmallBulletsHitShields);
+		player.SetVelocityY(.0f);
+		player.SetPositionY(groundCe->entity->GetPosition().y - player.GetHeight());
+	}
 }
 
 EPlayerState PlayerSitOnShield::GetState()
