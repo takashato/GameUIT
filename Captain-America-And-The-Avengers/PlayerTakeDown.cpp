55d7ec7d3476aa2b46781d9fa894043f8cc7ba6e
Bành Thanh Sơn <takashato@gmail.com>
2019-08-08 15:20:06 +0700

    Falling on Taken Down

--- Captain-America-And-The-Avengers/PlayerTakeDown.cpp
+++ Captain-America-And-The-Avengers/PlayerTakeDown.cpp
@@ -1,13 +1,15 @@
 #include "pch.h"
 #include "Player.h"
+#include "Ground.h"
+#include "BreakGround.h"
 #include "PlayerTakeDown.h"
 
 void PlayerTakeDown::Enter(Player& player, EPlayerState fromState, Data&& data)
 {
+	player.SetVelocityY(FALL_SPEED_VER_MAX);
 	player.mCurrentAni->Reset();
 	SoundManager::GetInstance().CPlaySound(SoundType::Death);
 	player.SetVelocityX(.0f);
-	player.SetVelocityY(.0f);
 }
 
 Data PlayerTakeDown::Exit(Player& player, EPlayerState toState)
@@ -38,4 +40,23 @@ void PlayerTakeDown::OnKeyUp(Player& player, BYTE code)
 
 void PlayerTakeDown::OnCollision(Player& player, std::vector<CollisionEvent*>& cEvent)
 {
+	player.mIsCollisionLeftRightSide = false;
+	bool collisionWithGround = false;
+	CollisionEvent* groundCe = nullptr;
+	for (auto ce : cEvent)
+	{
+		if (ce->entity->GetCollidableObjectType() == EPlatform)
+		{
+			if (ce->ny == -1.0f)
+			{
+				collisionWithGround = true;
+				groundCe = ce;
+			}
+		}
+	}
+	if (collisionWithGround)
+	{
+		player.SetVelocityY(.0f);
+		player.SetPositionY(groundCe->entity->GetPosition().y - player.GetHeight());
+	}
 }
